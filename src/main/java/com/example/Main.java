72ad62f6368f04240bfed94651c2db1784fe34d4
pijushcse <pijushcsecuet@gmail.com>
2019-11-28 16:30:22 -0500

    Update Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,24 +1,24 @@
 package com.example;
 
+import io.rollout.rox.server.Rox;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.ExecutionException;
+
 @SpringBootApplication
 @RestController
 public class Main {
  
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ExecutionException, InterruptedException {
 		Rox.setup("5de03af3676b362cfb74f712").get();
 		SpringApplication.run(Main.class, args);
 	}
  
 	@GetMapping("/ping")
 	public String ping() {
- 
 		return "Pong!!  " ;
 	}
-
- 
 }
